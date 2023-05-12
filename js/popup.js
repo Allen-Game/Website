95822b116baccbe4a46db1ef70f7b7343114d8e4
凌天 <49418651+lingtian152@users.noreply.github.com>
2023-05-12 18:26:12 +0000

    az

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,17 @@
 // 打开弹窗
 function openPopup(gameName, description) {
-    // 获取要加载的数据（这里仅做示例）
-    const title = gameName;
-    const content = description;
+    // 获取要加载的数据
+    const game_name = gameName; // 游戏名称
+    const content = description; // 游戏介绍
 
     // 将数据填充到弹窗的元素中
-    window.location.href = "Game.html#Name=" + title;
+    history.replaceState({}, document.title + `?title={game_name}`, window.location.pathname);
 
     // 加载图片并设置显示
     $("#popup-img").attr("src", $("#game-image").attr("src-data"));
 
     
-    $("#popup-title").text(title);
+    $("#popup-title").text(game_name);
     $("#popup-content").text(content);
 
     // 显示弹窗
@@ -21,7 +21,7 @@ function openPopup(gameName, description) {
 
 // 关闭弹窗
 function closePopup() {
-    history.replaceState({}, document.title, window.location.pathname);
+    history.replaceState({}, document.title, window.location.pathname); // 清除数据
 
     // 关闭弹窗
     $("#popup").css("display", "none");
