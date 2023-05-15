b822e3c7f73e654d689aafb91186625fe16d3a4b
lingtian152 <1553396053skwa@gmail.com>
2023-05-14 21:31:39 -0400

    az

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const nextConfig = {
+    // 有效的属性列表中选择合适的属性进行配置
     experimental: {
-      images: true,
+      appDocumentPreloading: true,
+    },
+    // 配置静态 HTML 导出
+    output: {
+      exportPathMap: async function () {
+        return {
+          '/': { page: '/' },
+        };
+      },
     },
   };
   
