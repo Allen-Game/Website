2230b44727533c85b8da063873d7bcdc10ad5455
lingtian152 <1553396053skwa@gmail.com>
2023-07-08 22:57:55 -0400

    updated UI

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ const Index = () => {
                         <Card.Img variant="top" src="https://tr.rbxcdn.com/e6225053b83a3364292aa14b5adbd0ea/420/420/Image/Png" />
                         <Card.Body style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
                             <Card.Title style={{ textAlign: "center" }}>Every Second +1 ki in DBZ</Card.Title>
-                            <Link href="https://www.roblox.com/games/12524383962/UPD9-Every-Second-1-Ki-in-DBZ" style={{ fontSize: "15px" }}>Play</Link>
+                            <Link href="https://www.roblox.com/games/12524383962/UPD9-Every-Second-1-Ki-in-DBZ" style={{ fontSize: "15px" }} className={Style.play}>Play</Link>
                         </Card.Body>
                     </Card>
 
@@ -38,14 +38,14 @@ const Index = () => {
                         <Card.Img variant="top" src="https://tr.rbxcdn.com/091981dd1705a8a13bc9134c93483635/720/720/Image/Png" />
                         <Card.Body style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
                             <Card.Title style={{ textAlign: "center" }}>Clicker Fighting Simulator</Card.Title>
-                            <Link href="https://www.roblox.com/games/12985134424/UPD-Clicker-Fighting-Simulator" style={{ fontSize: "15px" }}>Play</Link>
+                            <Link href="https://www.roblox.com/games/12985134424/UPD-Clicker-Fighting-Simulator" style={{ fontSize: "15px" }} className={Style.play}>Play</Link>
                         </Card.Body>
                     </Card>
                     <Card style={{ width: '18rem', backgroundColor: '#fff', margin: '15px 15px 15px 15px' }}>
                         <Card.Img variant="top" src="https://tr.rbxcdn.com/ca116716b6d59182792e5ea76d8eac6f/720/720/Image/Png" />
                         <Card.Body style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
                             <Card.Title style={{ textAlign: "center" }}>Naruto But Every Second +1 Chakra</Card.Title>
-                            <Link href="https://www.roblox.com/games/12995749346/UPD2-2-Naruto-But-Every-Second-1-Chakra" style={{ fontSize: "15px" }}>Play</Link>
+                            <Link href="https://www.roblox.com/games/12995749346/UPD2-2-Naruto-But-Every-Second-1-Chakra" style={{ fontSize: "15px" }} className={Style.play}>Play</Link>
                         </Card.Body>
 
                     </Card>
