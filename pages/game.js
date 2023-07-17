86d08c2df9afda909e5584501d79582e35c02e52
lingtian152 <1553396053skwa@gmail.com>
2023-07-16 20:42:23 -0400

    Refactored UI for mobile responsiveness

--- pages/game.js
+++ pages/game.js
@@ -8,6 +8,14 @@ import Style from '../public/css/game.module.css';
 const game = () => {
   return (
     <div>
+      <Head>
+        <title>Game</title>
+        <meta property="og:title" content="Home" key="title" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="icon/logo.svg" type="image/x-icon" />
+      </Head>
       <NavBar />
 
       <Game />
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import layout from '../app/layout';
 
 const Index = () => {
     return (
-        <div className={Style.body} data-theme="light">
+        <div className={Style.body}>
             <Head>
                 <title>Home</title>
                 <meta property="og:title" content="Home" key="title" />
@@ -24,10 +24,10 @@ const Index = () => {
             <Carousel />
 
             <div className={Style.container}>
-                <h2 className={Style.title}>Game</h2>
-                <Link className={Style.moreGame} href="./game">More Game</Link>
+                <h2 className={Style.title}>Game
+                    <Link className={Style.moreGame} href="./game">More Game</Link></h2>
                 <div className={Style.content}>
-                    <Card style={{ width: '18rem', backgroundColor: '#fff', margin: '15px 15px 15px 15px' }}>
+                    <Card style={{ width: '18rem', backgroundColor: '#fff', margin: '15px 15px 15px 15px', padding: "15px 15px 15px 15px" }}>
                         <Card.Img variant="top" src="https://tr.rbxcdn.com/e6225053b83a3364292aa14b5adbd0ea/420/420/Image/Png" />
                         <Card.Body style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
                             <Card.Title style={{ textAlign: "center" }}>Every Second +1 ki in DBZ</Card.Title>
@@ -35,14 +35,14 @@ const Index = () => {
                         </Card.Body>
                     </Card>
 
-                    <Card style={{ width: '18rem', backgroundColor: '#fff', margin: '15px 15px 15px 15px' }}>
+                    <Card style={{ width: '18rem', backgroundColor: '#fff', margin: '15px 15px 15px 15px', padding: "15px 15px 15px 15px" }}>
                         <Card.Img variant="top" src="https://tr.rbxcdn.com/091981dd1705a8a13bc9134c93483635/720/720/Image/Png" />
                         <Card.Body style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
                             <Card.Title style={{ textAlign: "center" }}>Clicker Fighting Simulator</Card.Title>
                             <Link href="https://www.roblox.com/games/12985134424/UPD-Clicker-Fighting-Simulator" style={{ fontSize: "15px" }} className={Style.play}>Play</Link>
                         </Card.Body>
                     </Card>
-                    <Card style={{ width: '18rem', backgroundColor: '#fff', margin: '15px 15px 15px 15px' }}>
+                    <Card style={{ width: '18rem', backgroundColor: '#fff', margin: '15px 15px 15px 15px', padding: "15px 15px 15px 15px" }}>
                         <Card.Img variant="top" src="https://tr.rbxcdn.com/ca116716b6d59182792e5ea76d8eac6f/720/720/Image/Png" />
                         <Card.Body style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
                             <Card.Title style={{ textAlign: "center" }}>Naruto But Every Second +1 Chakra</Card.Title>
