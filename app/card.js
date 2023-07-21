956ef8c94f90f8c57adab427c6955111b1d7c154
lingtian152 <1553396053skwa@gmail.com>
2023-07-21 19:44:57 -0400

    backup

--- app/card.js
+++ app/card.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import Style from '../public/css/game.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Script from 'next/script';
 
 const Game = () => {
     const [games, setGames] = useState([]);
@@ -22,9 +24,13 @@ const Game = () => {
 
     return (
         <div className={Style.container}>
+            <select id="checkType" className="dropdown_meun">
+                <option value="all">all</option>
+                <option value="Simulator">Simulator</option>
+            </select>
             <div className={Style.content}>
                 {games.map((game) => (
-                    <div key={game.id} className={Style.card} type={game.game_type}>
+                    <div key={game.id} id='game_card' className={Style.card + ' ' + game.game_type}>
                         <Image src={game.game_thum} width={420} height={420} alt={game.game_name} />
                         <div className={Style.cardTitle}>{game.game_name}</div>
                         <Link className={Style.playButton} href={game.game_url}>
